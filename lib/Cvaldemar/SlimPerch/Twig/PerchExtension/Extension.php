ac7ac87a06093905726eb5a99eb676710cc9f526
Casper Valdemar Poulsen <casper@grz.dk>
2013-10-02 16:51:04 +0400

    Added new perch methods

--- lib/Cvaldemar/SlimPerch/Twig/PerchExtension/Extension.php
+++ lib/Cvaldemar/SlimPerch/Twig/PerchExtension/Extension.php
@@ -9,6 +9,8 @@ class Extension extends \Twig_Extension
 		return array(
 			'perch_content'  => new \Twig_Function_Method($this, 'content'),
 			'perch_location' => new \Twig_Function_Method($this, 'location'),
+            'perch_content_create' => new \Twig_Function_Method($this, 'content_create'),
+            'perch_content_custom' => new \Twig_Function_Method($this, 'content_custom'),
 		);
 	}
 
@@ -21,6 +23,16 @@ class Extension extends \Twig_Extension
 	{
 		\PerchSystem::set_page($set_page);
 	}
+
+    public function content_create($key, $opts)
+    {
+        return perch_content_create($key, $opts);
+    }
+
+    public function content_custom($opts, $return)
+    {
+        return perch_content_custom($opts, $return);
+    }
 
 	public function getName()
 	{
