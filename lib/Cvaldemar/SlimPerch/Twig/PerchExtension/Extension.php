3e92debbba8074823ce1763e4281caf08b22700f
Casper Valdemar Poulsen <casper@grz.dk>
2013-10-06 11:45:54 +0400

    Added navigation to extension

--- lib/Cvaldemar/SlimPerch/Twig/PerchExtension/Extension.php
+++ lib/Cvaldemar/SlimPerch/Twig/PerchExtension/Extension.php
@@ -12,6 +12,8 @@ class Extension extends \Twig_Extension
             'perch_content_create' => new \Twig_Function_Method($this, 'content_create'),
             'perch_content_custom' => new \Twig_Function_Method($this, 'content_custom'),
             'perch_content_search' => new \Twig_Function_Method($this, 'content_search'),
+            'perch_pages_navigation' => new \Twig_Function_Method($this, 'pages_navigation'),
+            'perch_pages_navigation_text' => new \Twig_Function_Method($this, 'pages_navigation_text'),
 		);
 	}
 
@@ -38,6 +40,16 @@ class Extension extends \Twig_Extension
     public function content_search($key, $opts)
     {
         return perch_content_search($key, $opts);
+    }
+
+    public function pages_navigation($opts)
+    {
+        return perch_pages_navigation($opts);
+    }
+
+    public function pages_navigation_text()
+    {
+        return perch_pages_navigation_text();
     }
 
 	public function getName()
