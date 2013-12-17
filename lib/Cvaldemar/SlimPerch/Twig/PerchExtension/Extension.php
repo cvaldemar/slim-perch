f72ab3e8cbf2f5928d4d918b28f3b498814bd57c
Casper Valdemar Poulsen <casper@grz.dk>
2013-12-17 17:41:17 +0400

    Added perchsize filter for rewriting filenames to image-wXXXhYYY.ext

--- lib/Cvaldemar/SlimPerch/Twig/PerchExtension/Extension.php
+++ lib/Cvaldemar/SlimPerch/Twig/PerchExtension/Extension.php
@@ -4,7 +4,11 @@ namespace Cvaldemar\SlimPerch\Twig\PerchExtension;
 
 class Extension extends \Twig_Extension
 {
-	public function getFunctions()
+    /**
+     * Functions
+     * @return array
+     */
+    public function getFunctions()
 	{
 		return array(
 			'perch_content'  => new \Twig_Function_Method($this, 'content'),
@@ -56,6 +60,23 @@ class Extension extends \Twig_Extension
     public function pages_breadcrumbs($opts = array())
     {
         return perch_pages_breadcrumbs($opts);
+    }
+
+    /**
+     * Filters
+     * @return array
+     */
+    public function getFilters()
+    {
+        return array(
+            new \Twig_SimpleFilter('perchsize', array($this, 'perchsize')),
+        );
+    }
+
+    public function perchsize($path, $width = 200, $height = 100)
+    {
+        $p = pathinfo($path);
+        return $p['dirname'] . '/' . $p['filename'] . '-w' . $width . 'h' . $height . '.' . $p['extension'];
     }
 
 	public function getName()
